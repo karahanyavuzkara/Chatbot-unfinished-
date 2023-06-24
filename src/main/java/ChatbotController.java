8ba72209522314c4fc08afaf21737de4b0ee4184
nefretac <karahanyavuz61@gmail.com>
2023-06-25 02:02:08 +0300

    first commit

--- src/main/java/ChatbotController.java
+++ src/main/java/ChatbotController.java
@@ -1,53 +1,88 @@
-import org.springframework.ui.Model;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-
-@RestController
-
+import java.util.Properties;
 
 @Controller
-public class ChatbotController {
+public class ChatbotController extends Application {
 
     @Autowired
     private StanfordCoreNLP coreNLP;
-    @RequestMapping("/")
-    public String chatbotView() {
 
-        return "chatbot"; // Return the name of your HTML template
+    private TextField userInputField;
+    private Label chatArea;
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    @RequestMapping(value = "/analyze", method = RequestMethod.POST)
-    public String analyzeInput(@RequestParam("input") String userInput, Model model) {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Chatbot GUI");
+
+        // Create GUI components
+        userInputField = new TextField();
+        Button sendButton = new Button("Send");
+        chatArea = new Label();
+
+        // Set event handler for the send button
+        sendButton.setOnAction(event -> analyzeInput());
+
+        // Create a layout container and add components
+        VBox root = new VBox(10);
+        root.getChildren().addAll(chatArea, userInputField, sendButton);
+
+        // Create the scene and set it on the stage
+        Scene scene = new Scene(root, 400, 400);
+        primaryStage.setScene(scene);
+
+        // Initialize your StanfordCoreNLP instance
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, sentiment");
+        coreNLP = new StanfordCoreNLP(props);
+
+        primaryStage.show();
+    }
+
+    private void analyzeInput() {
+        String userInput = userInputField.getText();
+
         // Perform sentiment analysis using CoreNLP
         Annotation document = new Annotation(userInput);
         coreNLP.annotate(document);
 
         List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
         String sentiment = sentences.get(0).get(SentimentCoreAnnotations.SentimentClass.class);
+
         String response;
 
-        //Generating responses to user
-        if(userInput.toLowerCase().contains("hi"+ "Hello" + "Hey" + "hey" + "hello" + "Hi" )){
-            response = "Hello ! How can I help you ?";
+        // Generate response based on user input
+        if (userInput.toLowerCase().contains("hi") || userInput.toLowerCase().contains("hello")) {
+            response = "Hello! How can I assist you?";
+        } else {
+            response = "I'm sorry, I didn't understand your input.";
         }
-        else{
-            response = "I did not understand";
-        }
 
+        // Update the chat conversation
+        chatArea.setText("User: " + userInput + "\n");
+        chatArea.setText(chatArea.getText() + "Chatbot: " + response + "\n");
 
-        // Add the sentiment analysis result to the model
-        model.addAttribute(sentiment,"sentiment");
-        model.addAttribute(response,"response");
-
-        return "chatbot";
+        // Clear the user input field
+        userInputField.clear();
     }
 }
