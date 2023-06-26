15c282ac1b354fe654d52683777ecd7c319ffd0d
nefretac <karahanyavuz61@gmail.com>
2023-06-27 00:10:39 +0300

    first commit

--- src/main/java/ChatbotController.java
+++ src/main/java/ChatbotController.java
@@ -3,65 +3,24 @@ import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Properties;
 
-@Controller
-public class ChatbotController extends Application {
+public class ChatbotController {
 
-    @Autowired
     private StanfordCoreNLP coreNLP;
 
-    private TextField userInputField;
-    private Label chatArea;
-
     public static void main(String[] args) {
-        launch(args);
+        SpringApplication.run(ChatbotController.class, args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Chatbot GUI");
-
-        // Create GUI components
-        userInputField = new TextField();
-        Button sendButton = new Button("Send");
-        chatArea = new Label();
-
-        // Set event handler for the send button
-        sendButton.setOnAction(event -> analyzeInput());
-
-        // Create a layout container and add components
-        VBox root = new VBox(10);
-        root.getChildren().addAll(chatArea, userInputField, sendButton);
-
-        // Create the scene and set it on the stage
-        Scene scene = new Scene(root, 400, 400);
-        primaryStage.setScene(scene);
-
-        // Initialize your StanfordCoreNLP instance
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, sentiment");
-        coreNLP = new StanfordCoreNLP(props);
-
-        primaryStage.show();
-    }
-
-    private void analyzeInput() {
-        String userInput = userInputField.getText();
-
+    @RequestMapping(value = "/analyze", method = RequestMethod.POST)
+    public String analyzeInput(@RequestParam("input") String userInput) {
         // Perform sentiment analysis using CoreNLP
         Annotation document = new Annotation(userInput);
         coreNLP.annotate(document);
@@ -78,11 +37,43 @@ public class ChatbotController extends Application {
             response = "I'm sorry, I didn't understand your input.";
         }
 
-        // Update the chat conversation
-        chatArea.setText("User: " + userInput + "\n");
-        chatArea.setText(chatArea.getText() + "Chatbot: " + response + "\n");
+        // Create a JSON response object
+        ResponseObject responseObject = new ResponseObject(sentiment, response);
 
-        // Clear the user input field
-        userInputField.clear();
+        // Return the JSON response
+        return responseObject.toJson();
+    }
+}
+
+class ResponseObject {
+    private String sentiment;
+    private String response;
+
+    public ResponseObject(String sentiment, String response) {
+        this.sentiment = sentiment;
+        this.response = response;
+    }
+
+    // Getters and setters
+
+    public String getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(String sentiment) {
+        this.sentiment = sentiment;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    // Convert object to JSON string
+    public String toJson() {
+        return "{\"sentiment\":\"" + sentiment + "\", \"response\":\"" + response + "\"}";
     }
 }
--- src/main/java/org/example/ChatbotApplication.java
+++ src/main/java/org/example/ChatbotApplication.java
@@ -1,6 +1,28 @@
 package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Properties;
+
+
+
 @SpringBootApplication
 public class ChatbotApplication {
     public static void main( String[] args ) {
